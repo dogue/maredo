7ffe83b49b3ef7db1af1d938354c69c591d5a78d
Dogue <dogue@oddhelix.com>
2024-05-23 20:54:35 -0400

    remember that //go:embed is a thing

--- render.go
+++ render.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"bytes"
+	_ "embed"
 	bf "github.com/russross/blackfriday/v2"
 	"html/template"
 	"os"
 )
+
+//go:embed template.html
+var TEMPLATE string
 
 func renderPage(filename string, templData TemplData) (out string, err error) {
 	file, err := os.ReadFile(filename)
@@ -15,33 +19,8 @@ func renderPage(filename string, templData TemplData) (out string, err error) {
 	html := bf.Run(file)
 	templData.Body = template.HTML(html)
 	t := template.New("main")
-	t.Parse(RENDER_TEMPL)
+	t.Parse(TEMPLATE)
 	buf := bytes.Buffer{}
 	t.Execute(&buf, templData)
 	return buf.String(), nil
 }
-
-const (
-	RENDER_TEMPL = `<!DOCTYPE html>
-<html>
-
-<head>
-	<title>{{ .Title }}</title>
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.css">
-	<!-- <link rel="stylesheet" href="./tomorrow-night-eighties.css"> -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
-	{{ range .Langs -}}
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/{{ . }}.min.js"></script>
-	{{ end }}
-</head>
-
-<body>
-	{{ .Body }}
-	<script>hljs.highlightAll();</script>
-</body>
-
-</html>
-`
-)
