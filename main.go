378891c3667fbdaca96c68c389c565eb211b6bcb
Dogue <dogue@oddhelix.com>
2024-05-23 20:52:08 -0400

    embed template as const str

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
-import (
+import "fmt"
+
 // "github.com/adrg/xdg"
-)
 
 const VERSION = "0.0.1"
 
 func main() {
 	app := initCli()
 	_ = app
+	html, _ := renderPage(sourceFile, data)
+	fmt.Printf("%s\n", html)
 }
--- render.go
+++ render.go
@@ -14,8 +14,34 @@ func renderPage(filename string, templData TemplData) (out string, err error) {
 	}
 	html := bf.Run(file)
 	templData.Body = template.HTML(html)
-	t := template.Must(template.ParseFiles("template.html"))
+	t := template.New("main")
+	t.Parse(RENDER_TEMPL)
 	buf := bytes.Buffer{}
 	t.Execute(&buf, templData)
 	return buf.String(), nil
 }
+
+const (
+	RENDER_TEMPL = `<!DOCTYPE html>
+<html>
+
+<head>
+	<title>{{ .Title }}</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/styles/github-dark.css">
+	<!-- <link rel="stylesheet" href="./tomorrow-night-eighties.css"> -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/highlight.min.js"></script>
+	{{ range .Langs -}}
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/highlight.js/11.9.0/languages/{{ . }}.min.js"></script>
+	{{ end }}
+</head>
+
+<body>
+	{{ .Body }}
+	<script>hljs.highlightAll();</script>
+</body>
+
+</html>
+`
+)
